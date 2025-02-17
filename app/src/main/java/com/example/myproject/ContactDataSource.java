0fcb222fa11352219416ac273b39bc3aa7a7efcd
Ruth_Nure <rnure1@student.gsu.edu>
2025-02-17 12:01:01 -0500

    xml for complex list

--- app/src/main/java/com/example/myproject/ContactDataSource.java
+++ app/src/main/java/com/example/myproject/ContactDataSource.java
@@ -7,6 +7,7 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class ContactDataSource {
 
@@ -102,6 +103,39 @@ public class ContactDataSource {
         } catch (Exception e) {
             contactNames = new ArrayList<String>();
         } return contactNames;
+    }
+
+    public ArrayList<Contact> getContacts() {
+        ArrayList<Contact> contacts = new ArrayList<Contact>();
+
+        try {
+            String query = "Select * from contact";
+            Cursor cursor = database.rawQuery(query, null);
+
+            Contact newContact;
+            cursor.moveToFirst();
+            while (!cursor.isAfterLast()){
+                newContact = new Contact();
+                newContact.setContactID(cursor.getInt(0));
+                newContact.setContactName(cursor.getString(1));
+                newContact.setStreetAddress(cursor.getString(2));
+                newContact.setCity(cursor.getString(3));
+                newContact.setState(cursor.getString(4));
+                newContact.setZipCode(cursor.getString(5));
+                newContact.setPhoneNumber(cursor.getString(6));
+                newContact.setCellNumber(cursor.getString(7));
+                newContact.seteMail(cursor.getString(8));
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTimeInMillis(Long.valueOf(cursor.getString(9)));
+                newContact.setBirthday(calendar);
+                contacts.add(newContact);
+                cursor.moveToNext();
+            }
+            cursor.close();
+
+        } catch (Exception e) {
+            contacts = new ArrayList<Contact>();
+        } return contacts;
     }
 
 
